6792a22808dbc3fad11216e5a65825a0096fe00b
Bruno Luiz da Silva <bruno.silvagj@gmail.com>
2015-12-02 16:44:33 -0200

    Ch 13.

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,5 +1,11 @@
 package br.com.casadocodigo.loja.conf;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,10 +14,16 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import br.com.casadocodigo.loja.viewresolver.JsonViewResolver;
+
 @EnableWebMvc
+@EnableCaching
 @ComponentScan(basePackages = "br.com.casadocodigo.loja")
 public class AppWebConfiguration {
 
@@ -41,5 +53,23 @@ public class AppWebConfiguration {
 		registrar.registerFormatters(conversionService);
 		
 		return conversionService;
+	}
+	
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager();
+	}
+	
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		List<ViewResolver> resolvers = new ArrayList<>();
+		
+		resolvers.add(internalResourceViewResolver());
+		resolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(resolvers);
+		resolver.setContentNegotiationManager(manager);
+		return resolver;
 	}
 }
--- src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
+++ src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
@@ -1,19 +1,21 @@
 package br.com.casadocodigo.loja.controller;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.com.casadocodigo.loja.conf.ProductValidator;
 import br.com.casadocodigo.loja.daos.ProductDAO;
 import br.com.casadocodigo.loja.models.BookType;
 import br.com.casadocodigo.loja.models.Product;
@@ -43,12 +45,14 @@ public class ProductsController {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
+	@Cacheable(value="books")
 	public String list(Model model) {
 		model.addAttribute("products", productDAO.list());
 		return "products/list";
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
+	@CacheEvict(allEntries = true, value = "books")
 	public String save(@Valid Product product, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
 		System.out.println("Cadastrando o produto");
 		
@@ -62,5 +66,11 @@ public class ProductsController {
 		productDAO.save(product);
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso");
 		return "redirect:produtos";
+	}
+	
+	@RequestMapping(value = "json", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Product> listJson() {
+		return productDAO.list();
 	}
 }
